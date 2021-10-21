71f0c1df47a34c0ba32cc80b5cc81f7c671ca371
SophieGutjahr <angutjahr@edu.aau.at>
2021-10-21 17:54:21 +0200

    TestCase 1

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,4 +1,11 @@
 public class Einstiegsbeispiel {
 
-  
+    public int getInnerProdukt (int [] a, int [] b){
+        int ergebnis= 0;
+
+        for (int i = 0; i< a.length; i++){
+            ergebnis= ergebnis + a[i]*b[i];
+        }
+        return ergebnis;
+    }
 }
---
+++ src/test/java/EinstiegsbeispielTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EinstiegsbeispielTest {
+
+
+    Einstiegsbeispiel skalarproduct = new Einstiegsbeispiel();
+
+    @Test
+    public void ergebnisRichtig1(){
+        int []a = {1,1,1,1,1,1};
+        int []b = {2,2,2,2,2,2};
+        int ergebnis = 12;
+        assertEquals(skalarproduct.getInnerProdukt(a,b),ergebnis);
+
+    }
+    @Test
+    public void ErgebnisRichtig2(){
+        int [] c= {1,1,1,1};
+        int [] d = {3,3,3,3};
+        
+    }
+
+}
